e1f047b835a1e6c5bbe064ee678d0292d5113660
Luis Arce <luis.arce22@gmail.com>
2018-10-05 16:57:03 -0400

    Move the ball on the field

--- src/app/cancha-container/cancha-container.component.ts
+++ src/app/cancha-container/cancha-container.component.ts
@@ -10,14 +10,56 @@ export class CanchaContainerComponent implements OnInit {
   vertical: boolean;
   svg: any;
   ball: any;
+  minArea: any;
+  curColor: any;
+  images: any;
   constructor() {
     this.vertical = false;
   }
 
   ngOnInit() {
   }
-  
+
+  animation() {
+    let self = this;
+    this.svg.transition()
+      .duration(7500)
+      .tween('precision', function () {
+        let area = d3.interpolateRound(100, 200);
+        let color = d3.interpolateRgb('rgb(255,0,255)', 'rgb(0,255,0)');
+        return function (t) {
+          self.minArea = area(t);
+          self.curColor = color(t);
+          self.render();
+        };
+      })
+      .transition()
+      .duration(7500)
+      .tween('precision', function () {
+        let area = d3.interpolateRound(200, 100);
+        let color = d3.interpolateRgb('rgb(0,255,0)', 'rgb(255,0,255)');
+        return function (t) {
+          self.minArea = area(t);
+          self.curColor = color(t);
+          self.render();
+        };
+      })
+      .transition()
+      .duration(2500)
+      .each('end', self.animation);
+  }
+
+  render() {
+    this.images.attr('x', this.minArea);
+  }
+
   changeOrientation() {
     this.vertical = !this.vertical;
+  }
+
+  clickAction() {
+    this.svg = d3.select('#field1');
+    this.images = d3.select('#image1');
+    this.animation();
   }
 }

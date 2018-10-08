388fe7efa2069e56014cd3494d6ce842641aec52
Luis Arce <luis.arce22@gmail.com>
2018-10-08 13:42:45 -0400

    Move ball to cliked image

--- src/app/cancha-container/cancha-container.component.ts
+++ src/app/cancha-container/cancha-container.component.ts
@@ -11,46 +11,58 @@ export class CanchaContainerComponent implements OnInit {
   svg: any;
   ball: any;
   minArea: any;
+  minArea2: any;
   curColor: any;
   images: any;
+
+  destiny: any;
+  origin: any;
+
   constructor() {
     this.vertical = false;
+    this.origin = {x: 1, y: 120};
+    this.destiny = {x: 120, y: 120};
   }
 
   ngOnInit() {
   }
 
   animation() {
-    let self = this;
+
+    if (!this.svg) {
+      this.svg = d3.select('#field1');
+      this.images = d3.select('#image1');
+    }
+
+    const self = this;
     this.svg.transition()
-      .duration(7500)
+      .duration(2000)
       .tween('precision', function () {
-        let area = d3.interpolateRound(100, 200);
+        let area = d3.interpolateRound(self.origin.x, self.destiny.x);
+        let area2 = d3.interpolateRound(self.origin.y, self.destiny.y);
+
         let color = d3.interpolateRgb('rgb(255,0,255)', 'rgb(0,255,0)');
         return function (t) {
           self.minArea = area(t);
+          self.minArea2 = area2(t);
           self.curColor = color(t);
           self.render();
         };
       })
       .transition()
-      .duration(7500)
-      .tween('precision', function () {
-        let area = d3.interpolateRound(200, 100);
-        let color = d3.interpolateRgb('rgb(0,255,0)', 'rgb(255,0,255)');
-        return function (t) {
-          self.minArea = area(t);
-          self.curColor = color(t);
-          self.render();
-        };
-      })
-      .transition()
-      .duration(2500)
-      .each('end', self.animation);
+      .duration(2000);
   }
 
   render() {
-    this.images.attr('x', this.minArea);
+    this.images.attr('x', this.minArea).attr('y', this.minArea2);
+  }
+
+  goHere(event) {
+    this.origin.x = this.destiny.x;
+    this.origin.y = this.destiny.y;
+    this.destiny.x = event.offsetX;
+    this.destiny.y = event.offsetY;
+    this.animation();
   }
 
   changeOrientation() {
@@ -58,8 +70,6 @@ export class CanchaContainerComponent implements OnInit {
   }
 
   clickAction() {
-    this.svg = d3.select('#field1');
-    this.images = d3.select('#image1');
     this.animation();
   }
 }
